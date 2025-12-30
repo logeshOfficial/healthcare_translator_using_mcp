0ef2a108a99ce94c9764c2f8d3aece4ced05f791
logeshOffical <iamlogeshwaran.info@gmail.com>
2025-12-30 16:02:07 +0530

    "small changes"

--- api_server.py
+++ api_server.py
@@ -1,22 +1,27 @@
 from fastapi import FastAPI, HTTPException
 from mcp_server import translate_medical_text
+from pydantic import BaseModel
 
 app = FastAPI(title="Healthcare Translator API")
 
+class TranslateRequest(BaseModel):
+    text: str
+    target_language: str
+    
 # Health check endpoint (required by Render)
 @app.get("/")
 def root():
     return {"status": "API running"}
 
 @app.post("/translate")
-def translate(payload: dict):
+def translate(req: TranslateRequest):
     try:
-        text = payload["text"]
-        target_language = payload["target_language"]
+        text = req.text
+        target_language = req.target_language
 
         result = translate_medical_text(text, target_language)
 
-        return {"text": result}
+        return {"text": result.text}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
