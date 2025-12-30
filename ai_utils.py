05a48cf95e61166305ef97911c01ea621e8973c4
logeshOffical <iamlogeshwaran.info@gmail.com>
2025-12-30 12:13:03 +0530

    "code"

--- ai_utils.py
+++ ai_utils.py
@@ -1,6 +1,7 @@
 import requests
 
-API_URL =  "https://healthcare-translator-using-mcp.onrender.com"
+API_URL = "https://healthcare-translator-using-mcp.onrender.com/mcp/call"
+# API_URL =  "https://healthcare-translator-using-mcp.onrender.com"
 # API_URL = "http://localhost:8000/translate"
 
 def translate_medical_text(text, target_language):
